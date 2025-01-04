c7afcef12e3a44bddbfa25ba1ec9210f07f45f98
Rafael de jesus <Rafaeldejesus200000@gmail.com>
2025-01-04 12:39:36 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import qrcode
+
+def create_qr_code(data, filename="qrcode.png", box_size=10, border=4):
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=box_size,
+        border=border,
+    )
+    qr.add_data(data)
+    qr.make(fit=True)
+
+    img = qr.make_image(fill_color="black", back_color="white")
+    img.save(filename)
+
+create_qr_code("https://www.amazon.com", "amazon.png")
